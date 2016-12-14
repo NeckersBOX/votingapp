96fb95659a6dfd04858b2c1d0e2d212154048c3f
Davide Francesco Merico <hds619@gmail.com>
2016-12-14 18:59:31 +0100

    Rename SignUp->SignUpPage component

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import IndexPage from './components/IndexPage';
 import Layout from './components/Layout';
-import SignUp from './components/SignUp';
+import SignUp from './components/SignUpPage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={IndexPage} />
-    <Route path="signup" component={SignUp} />
+    <Route path="signup" component={SignUpPage} />
     <Route path="*" component={NotFoundPage} />
   </Route>
 );
